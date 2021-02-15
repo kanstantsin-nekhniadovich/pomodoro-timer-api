42211900b4dba12fea334d0931b1ae44e81fcc23
kanstantsin-nekhniadovich <konstantyn.niechniedowicz@gmail.com>
2021-02-15 07:45:50 +0100

    fix: get porject for user

--- src/resolvers/project/query.ts
+++ src/resolvers/project/query.ts
@@ -1,24 +1,26 @@
 import { Project } from '@typings';
 import { isDefined } from '../../utils/isDefined';
+import { getUserIdFromAuthorizationHeader } from '../../utils/getUserIdFromAuthorizationHeader';
 
 export const Query = {
-  projects: (_parent: unknown, args: Project.Query.Projects, { prisma }: Context): Promise<Project[]> => {
+  projects: (_parent: unknown, args: Project.Query.Projects, { prisma, request }: Context): Promise<Project[]> => {
     const { query, skip, after, before, first, last } = args;
+    const id = getUserIdFromAuthorizationHeader(request);
+
+    const where = { owner: { id } };
     const optArgs = { last, skip, first, after, before };
 
     if (isDefined(query)) {
-      Object.assign(optArgs, {
-        where: {
-          OR: [{
-            title_contains: query,
-          }, {
-            note_contains: query,
-          }],
-        },
+      Object.assign(where, {
+        OR: [{
+          title_contains: query,
+        }, {
+          note_contains: query,
+        }],
       });
     }
 
-    return prisma.projects(optArgs);
+    return prisma.projects({ ...optArgs, where });
   },
   project: (_parent: unknown, args: UniqueIdPayload, { prisma }: Context): Promise<Nullable<Project>> => {
     return prisma.project({ id: args.id });
