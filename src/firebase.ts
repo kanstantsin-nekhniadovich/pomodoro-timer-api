2d6a5a9ef7af96f6d8e705b9ad892f5c058503df
kanstantsin-nekhniadovich <konstantyn.niechniedowicz@gmail.com>
2021-05-02 16:18:54 +0200

    fix: private key for heroku

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp, credential } from 'firebase-admin';
 
 initializeApp({
   credential: credential.cert({
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     projectId: process.env.FIREBASE_PROJECT_ID,
   }),
